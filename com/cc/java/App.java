002af55a5d084db6703aa47e4e1710224b3876d6
Michael <atlasbook@web.de>
2025-11-21 12:12:56 +0100

    01 / Felder / Typkonventierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,12 +8,27 @@ public class App {
         output("Blick von aussen: " + cat);
         output("Blick von innen: " + cat.getInstanceVariable());
 
+        cat.firstName = "Grizabella";
+        cat.furColor = "white";
+        cat.age = 29;
+
+        output(cat.firstName);
+        output(cat.furColor);
+        output(Integer.toString(cat.age)); // Explizite Typumwandlung!
+
         output("-----------------------");
 
         Cat cat2 = new Cat();
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVariable());
 
+        cat.firstName = "Alonzo";
+        cat.furColor = "grey";
+        cat.age = 35;
+
+        output(cat2.firstName);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age)); // Explizite Typumwandlung!
     }
 
   
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,11 +2,14 @@ package com.cc.java;
 
 public class Cat {
 
+    // Eigenschaften / Felder / Attribute
+    String firstName;
+    String furColor;
+    int age; // kleingeschriebene Datentypen haben keine Klassen / Primitives
+
     // return der Instanzvariablen / Adresse
     public Cat getInstanceVariable(){
        return this;
     }
     
-
-
 }
